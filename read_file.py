f8dcdb6eef9c82cd0fb8cd4aff320394322931e7
Jason Lo <lochenan3@gmail.com>
2023-05-25 19:42:35 +0800

    fix add word count

--- read_file.py
+++ read_file.py
@@ -1,4 +1,15 @@
+count = 0
+data =[]
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
 
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+print(data[0])
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
@@ -29,18 +40,7 @@ for d in data:
     bad.append('bad' in d)
 
 
-count = 0
-data =[]
-with open('reviews.txt', 'r') as f:
-    for line in f:
-        data.append(line)
-        count += 1
-        if count % 1000 == 0:
-            print(len(data))
 
-print('檔案讀取完了，總共有', len(data), '筆資料')
-
-print(data[0])
 
 
 # 文字記數
